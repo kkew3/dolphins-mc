396af0cac4e83f8e2d878a658aa3a9fdba9c52bc
Kaiwen Wu <kps6326@hotmail.com>
2018-08-21 00:23:26 -0700

    Added multiprocessing for efficiency when loading dataset

--- lib/vdata.py
+++ lib/vdata.py
@@ -2,6 +2,7 @@
 Defines the class representing the video dataset
 """
 import json
+import multiprocessing
 import os
 import re
 import hashlib
@@ -16,10 +17,37 @@ from PIL import Image
 import utils
 
 
+DATASET_NOT_FOUND = 0x01
+DATASET_CORRUPTED = 0x02
+
+
+def check_file_integrity(checksum_line):
+    """
+    Check integrity (SHA-1) of a single file.
+    :param checksum_line: a tuple of form ``(filename, expected_hex)``,
+           where ``filename`` is the absolute path of the file to check, and
+           ``expected_hex`` is the hex string of the expected SHA-1 hash
+           of the block
+    :return: 0 if the checking passes, and the bit-wise error signal (defined
+             as ``DATASET_NOT_FOUND`` and ``DATASET_CORRUPTED``) otherwise
+    :rtype: int
+    """
+    hashbuff = hashlib.sha1()
+    filename, expected_hex = checksum_line
+    try:
+        with open(filename, 'rb') as infile:
+            for block in iter(lambda: infile.read(1024 * 1024), b''):
+                hashbuff.update(block)
+        if hashbuff.hexdigest() != expected_hex:
+            return DATASET_CORRUPTED
+        return 0
+    except IOError:
+        return DATASET_NOT_FOUND | DATASET_CORRUPTED
+
 class VideoDataset(Dataset):
     """
     Represents a HDF5 block-based video dataset, as built by
-    "${repo-root}/bin/build-video-dataset.py". The dataset returns the
+    ``${repo-root}/bin/build-video-dataset.py``. The dataset returns the
     corresponding frame tensor when requesting the frame ID (indexed from zero).
     The dataset is not supervised.
     """
@@ -73,7 +101,7 @@ class VideoDataset(Dataset):
             frame = self.transform(frame)
         return frame
 
-    def check_integrity(self):
+    def check_integrity(self, workers=4):
         video_name = os.path.basename(self.root)
         checksumfile = os.path.join(self.root, video_name + '.sha1')
         checksums = dict()
@@ -81,16 +109,22 @@ class VideoDataset(Dataset):
             for line in infile:
                 expected, block_filename = line.strip().split(' *')
                 checksums[os.path.join(self.root, block_filename)] = expected
-        for filename in checksums:
-            hashbuff = hashlib.sha1()
-            try:
-                with open(filename, 'rb') as infile:
-                    for block in iter(lambda: infile.read(1024 * 1024), b''):
-                        hashbuff.update(block)
-                if hashbuff.hexdigest() != checksums[filename]:
+        if workers <= 1:
+            for checksum_line in checksums.items():
+                errno = check_file_integrity(checksum_line)
+                if errno == DATASET_CORRUPTED | DATASET_NOT_FOUND:
+                    raise RuntimeError('Dataset not found or corrupted')
+                elif errno == DATASET_CORRUPTED:
                     raise RuntimeError('Dataset corrupted')
-            except IOError:
+        else:
+            # fail fast multiprocessing is too difficult to implement ...
+            pool = multiprocessing.Pool(workers)
+            errnos = pool.map(check_file_integrity, checksums.items())
+            if DATASET_NOT_FOUND | DATASET_CORRUPTED in errnos:
                 raise RuntimeError('Dataset not found or corrupted')
+            elif DATASET_CORRUPTED in errnos:
+                raise RuntimeError('Dataset corrupted')
+            pool.close()
 
     def locate_block(self, frame_id):
         """
@@ -122,14 +156,14 @@ class VideoDataset(Dataset):
 class PairedVideoDataset(Dataset):
     """
     Represents a pairing of two HDF5 block-based video datasets, as built by
-    "${repo-root}/bin/build-video-dataset.py". The dataset returns a pair of
+    ``${repo-root}/bin/build-video-dataset.py``. The dataset returns a pair of
     frame tensor when requesting the frame ID (indexed from zero).
     """
     def __init__(self, dataset1, dataset2):
         """
-        :param dataset1: one `VideoDataset` instance representing one video
+        :param dataset1: one ``VideoDataset`` instance representing one video
         :type dataset1: VideoDataset
-        :param dataset2: another `VideoDataset` instance representing another
+        :param dataset2: another ``VideoDataset`` instance representing another
                video
         :type dataset2: VideoDataset
         """
