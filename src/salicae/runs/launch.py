cbaaea8f3cacdb2db4078689420aa8134114ac32
Kaiwen Wu <kps6326@hotmail.com>
2018-09-27 23:53:01 -0700

    Initial commit

---
+++ src/salicae/runs/launch.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import torch
+import torchvision.transforms as trans
+
+import train
+import vmdata
+import more_trans
+import salicae
+
+
+if __name__ == '__main__':
+    root = vmdata.prepare_dataset_root(9, (8, 0, 0))
+    normalize = trans.Normalize(*vmdata.get_normalization_stats(root))
+    dset = vmdata.VideoDataset(root, max_mmap=4, max_gzcache=10,
+                               transform=trans.Compose([
+                                   more_trans.MedianBlur(),
+                                   trans.ToTensor(),
+                                   normalize,
+                               ]))
+    net = salicae.SaliencyCAE(vgg_arch='Ashallow', batch_norm=False)
+    device = torch.device('cuda')
+    batch_size = 16
+    lasso_strength = 1.
+    savedir = 'save'
+    statdir = 'stats'
+    train.train(net, dset, device, batch_size, lasso_strength,
+                statdir, savedir)
