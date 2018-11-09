fc55b9a0bc32020c776e5fb9bd67fed7412e06aa
Kaiwen Wu <kps6326@hotmail.com>
2018-11-09 14:35:28 -0800

    Added default trainer

--- lib/trainlib.py
+++ lib/trainlib.py
@@ -3,13 +3,15 @@ Common training utilities
 """
 
 import os
-import contextlib
 import logging
+import re
+from datetime import datetime
+import collections
 
 import numpy as np
 import torch
 import torch.nn as nn
-from typing import Any, Union, Callable
+from typing import Any, Union, Callable, Iterable, Tuple, Dict
 
 
 def action_fired(fired: Union[int, Callable[[Any], bool]]) -> Callable[[Any], bool]:
@@ -27,8 +29,10 @@ def action_fired(fired: Union[int, Callable[[Any], bool]]) -> Callable[[Any], bo
             logger.warn('Expect `fired` at least 1 if int but got {}; '
                         'converted to 1'.format(fired))
             fired = 1
+
         def _fired(progress: int):
             return progress % fired == 0
+
         return _fired
     return fired
 
@@ -39,8 +43,8 @@ class CheckpointSaver(object):
     """
 
     def __init__(self, net: nn.Module, savedir: str,
-                 checkpoint_tmpl: str='checkpoint_{0}.pth',
-                 fired: Union[int, Callable[[Any], bool]]=10):
+                 checkpoint_tmpl: str = 'checkpoint_{0}.pth',
+                 fired: Union[int, Callable[[Any], bool]] = 10):
         """
         :param net: the network to save states; the network should already been
                in the target device
@@ -58,23 +62,48 @@ class CheckpointSaver(object):
             raise TypeError('Expected torch.nn.Module of `net` but got: {}'
                             .format(type(net)))
 
+        savedir = os.path.normpath(savedir)
         os.makedirs(savedir, exist_ok=True)
 
         self.net = net
         self.fired = action_fired(fired)
         self.checkpoint_tmpl = checkpoint_tmpl
         self.savedir = savedir
+        self._logger = logging.getLogger(type(self).__name__)
 
     def __call__(self, progress):
         """
         Save checkpoint as needed.
 
         :param progress: see ``help(type(self).__init__)``
+        :return: the file written if fired; otherwise None
         """
         if self.fired(progress):
             name = self.checkpoint_tmpl.format(*progress)
             tofile = os.path.join(self.savedir, name)
             torch.save(self.net.state_dict(), tofile)
+            self._logger.debug('{} written at progress {}'
+                               .format(tofile, progress))
+            return tofile
+
+
+class CheckpointLoader(object):
+    def __init__(self, net: nn.Module, savedir: str,
+                 checkpoint_tmpl: str = 'checkpoint_{0}.pth',
+                 map_location: str = 'cpu'):
+        self.net = net
+        self.savedir = savedir
+        self.checkpoint_tmpl = checkpoint_tmpl
+        self.map_location = map_location
+        self._logger = logging.getLogger(type(self).__name__)
+
+    def __call__(self, progress):
+        checkpoint_name = self.checkpoint_tmpl.format(*progress)
+        fromfile = os.path.join(self.savedir, checkpoint_name)
+        state_dict = torch.load(fromfile, map_location=self.map_location)
+        self.net.load_state_dict(state_dict)
+        self._logger.debug('progress {} loaded from {}'
+                           .format(progress, fromfile))
 
 
 class StatSaver(object):
@@ -84,7 +113,7 @@ class StatSaver(object):
 
     def __init__(self, statdir: str,
                  statname_tmpl='stats_{0}.npz',
-                 fired: Union[int, Callable[[Any], bool]]=10):
+                 fired: Union[int, Callable[[Any], bool]] = 10):
         """
         :param statdir: the directory under which to write statistics npz
                files; if not exists, it will be created automatically
@@ -96,11 +125,13 @@ class StatSaver(object):
                (by ``str.format``), should accept exactly one positional
                argument the same as that passed to ``fired``
         """
+        statdir = os.path.normpath(statdir)
         os.makedirs(statdir, exist_ok=True)
 
         self.fired = action_fired(fired)
         self.statname_tmpl = statname_tmpl
         self.statdir = statdir
+        self.logger = logging.getLogger(type(self).__name__)
 
     def __call__(self, progress, **stat_dict):
         """
@@ -109,9 +140,265 @@ class StatSaver(object):
         :param progress: see ``help(type(self).__init__)``
         :param stat_dict: dict of floats or lists of floats, i.e. the
                non-cumulative statistics at the progress
+        :return: the file written if fired; otherwise None
         """
         if self.fired(progress):
             name = self.statname_tmpl.format(*progress)
             tofile = os.path.join(self.statdir, name)
             _stat_dict = {k: np.array(stat_dict[k]) for k in stat_dict}
             np.savez(tofile, **_stat_dict)
+            self.logger.debug(
+                    '{} written at progress {}'.format(tofile, progress))
+            return tofile
+
+
+class StatLoader(object):
+    def __init__(self, statdir: str,
+                 statname_tmpl: str = 'stats_{0}.npz'):
+        """
+        :param statdir: the directory under which to write statistics npz
+               files; if not exists, it will be created automatically
+        :param statname_tmpl: the stat npz file basename template
+               (by ``str.format``)
+        :param statname_pattern: the stat npz file basename regex pattern;
+               required if
+        :param betterthan:
+        """
+        self.statdir = statdir
+        self.statname_tmpl = statname_tmpl
+        self._logger = logging.getLogger(type(self).__name__)
+
+    def __call__(self, progress) -> Dict[str, np.ndarray]:
+        statname = self.statname_tmpl.format(*progress)
+        fromfile = os.path.join(self.statdir, statname)
+        data = np.load(fromfile)
+        self._logger.debug('progress {} loaded from {}'
+                           .format(progress, fromfile))
+        data = {k: data[k] for k in data.keys()}
+        return data
+
+
+class BasicTrainer(object):
+    timestamp_format = '%Y%m%d%H%M'  # e.g. 201811051438
+
+    def __init__(self, net: nn.Module, max_epoch: int = 1, device: str = 'cpu'):
+        r"""
+        :param net: the network to train
+        :param max_epoch: maximum epoch to train, where an epoch is defined as
+               a complete traversal of the underlying dataset
+        :param device: where to train the network, choices:
+               { cpu, cuda(:\d+)? }
+
+        Guideline to override this method:
+
+            - specify where the ``basedir`` of all other directories defined
+              below. If a directory has been specified, the ``basedir`` will
+              have no effect on that directory. If ``basedir`` is specified
+              but not a particular directory, then it will be set to
+              ``basedir/{default_directiory_name}``.
+            - specify where to store the saved checkpoints (``savedir``) and
+              the saved statistics (``statdir``) during training
+            - specify where to store the saved statistics (``statdir_eval``)
+              during evaluation on validation set
+            - specify how the saving of checkpoints and statistics is
+              triggered during training (``fired``, of signature
+              ``fired(Tuple[int, int]) -> bool``, that accepts a tuple of
+              ``(epoch_id, minibatch_id)`` and returns whether or not to
+              trigger the saving after current (epoch, minibatch)
+            - specify how the saving of statistics is triggered during
+              evaluation on validation set (``fired_eval``, definition the same
+              as ``fired``)
+            - specify the statistics names (``stat_names``), in the same order
+              as returned by either ``train_once`` or ``eval_once``; leaving
+              it as is makes the statistics anonymous
+        """
+        self.device = device
+        self.net = net.to(device)
+        self.max_epoch = max_epoch
+        self.train_stages = ('train', 'eval')
+        self.stage = None  # current training stage
+
+    @property
+    def default_basedir(self):
+        """
+        The default base directory of checkpoints and statistics, in form of
+        "runs-{timestamp}", with ``timestamp`` of datetime format
+        ``type(self).timestamp_format``.
+        """
+        return 'runs-{}'.format(datetime.today().strftime(
+                type(self).timestamp_format))
+
+    # noinspection PyUnresolvedReferences,PyAttributeOutsideInit
+    def init_monitors(self):
+        """
+        Initialize CheckpointSaver and StatSaver as per settings in
+        ``__init__``.
+        """
+        basedir = self.basedir if hasattr(
+                self, 'basedir') else self.default_basedir
+        defaults = {
+            'statdir': os.path.join(basedir, 'stat'),
+            'savedir': os.path.join(basedir, 'save'),
+            'fired': (lambda progress: True),
+            'statdir_eval': os.path.join(basedir, 'stat_eval'),
+            'fired_eval': (lambda progress: True),
+        }
+        for k, v in defaults.items():
+            if not hasattr(self, k):
+                setattr(self, k, v)
+
+        # deferred instantiation
+        self._statsaver = lambda: StatSaver(self.statdir, fired=self.fired,
+                                            statname_tmpl='stats_{0}_{1}.npz')
+        self._checkpointsaver = lambda: CheckpointSaver(self.net, self.savedir, fired=self.fired,
+                                                        checkpoint_tmpl='checkpoint_{0}_{1}.pth')
+        self._statsaver_eval = lambda: StatSaver(self.statdir_eval, fired=self.fired_eval,
+                                                 statname_tmpl='stats_{0}_{1}.npz')
+        self.statsaver = None
+        self.checkpointsaver = None
+        self.statsaver_eval = None
+
+    def train_once(self, inputs, targets) -> Tuple[Any]:
+        """
+        Train for one minibatch. The return type of this method should be the
+        same as that of ``validate_once``.
+
+        :param inputs: the inputs
+        :param targets: the targets
+        :return: anything that will be saved as statistics
+        """
+        raise NotImplementedError()
+
+    def eval_once(self, inputs, targets) -> Tuple[Any]:
+        """
+        Evaluate (in validation set) for one (mini)batch. The return type of
+        this method should be the same as that of ``train_once``.
+
+        :param inputs: the inputs
+        :param targets: the targets
+        :return: anything that will be saved as statistics
+        """
+        raise NotImplementedError()
+
+    def get_trainloader(self) -> Iterable[Tuple[torch.Tensor, torch.Tensor]]:
+        """
+        :return: the training set loader of input-target pairs
+        """
+        raise NotImplementedError()
+
+    def get_evalloader(self) -> Iterable[Tuple[torch.Tensor, torch.Tensor]]:
+        """
+        :return: the validation set loader of input-target pairs
+        """
+        raise NotImplementedError()
+
+    def before_batch_train(self):
+        """
+        Callback before each batch of the training loop.
+        """
+        pass
+
+    def after_batch_train(self):
+        """
+        Callback after each batch of the training loop.
+        """
+        pass
+
+    def before_batch_eval(self):
+        """
+        Callback before each batch of the evaluation loop.
+        """
+        pass
+
+    def after_batch_eval(self):
+        """
+        Callback after each batch of the evaluation loop.
+        """
+        pass
+
+    def before_epoch(self):
+        """
+        Callback before each epoch.
+        """
+        pass
+
+    def after_epoch(self):
+        """
+        Callback after each epoch.
+        """
+        pass
+
+    def setup(self):
+        """
+        Callback before ``run`` and after ``__init__``. Default to initializing
+        checkpoing and statistics savers.
+        """
+        self.init_monitors()
+
+    def teardown(self, error=None):
+        """
+        Callback before the return of ``run``, whether or not successful return
+        or unsuccessful one.
+
+        :param error: the cause of the return, or ``None`` if there's no error
+        """
+        pass
+
+    def run(self):
+        logger = logging.getLogger(type(self).__name__ + '.run')
+        logger.debug('Initializing')
+        self.setup()
+
+        try:
+            for epoch in range(self.max_epoch):
+                self.before_epoch()
+                for self.stage in self.train_stages:
+                    if self.stage == 'train':
+                        self.net.train()
+                        for batch, (inputs, targets) in enumerate(self.get_trainloader()):
+                            self.before_batch_train()
+                            stats = self.train_once(inputs, targets)
+                            stats_to_log = self._organize_stats(stats)
+                            logger.info('epoch{}/train batch{}: {}'
+                                        .format(epoch, batch, list(stats_to_log.items())))
+                            if self.statsaver is None:
+                                self.statsaver = self._statsaver()
+                            self.statsaver((epoch, batch), **stats_to_log)
+                            if self.checkpointsaver is None:
+                                self.checkpointsaver = self._checkpointsaver()
+                            self.checkpointsaver((epoch, batch))
+                            self.after_batch_train()
+                    else:
+                        self.net.eval()
+                        for batch, (inputs, targets) in enumerate(self.get_evalloader()):
+                            self.before_batch_eval()
+                            stats = self.eval_once(inputs, targets)
+                            stats_to_log = self._organize_stats(stats)
+                            logger.info('epoch{}/eval batch{}: {}'
+                                        .format(epoch, batch, list(stats_to_log.items())))
+                            if self.statsaver_eval is None:
+                                self.statsaver_eval = self._statsaver_eval()
+                            self.statsaver_eval((epoch, batch), **stats_to_log)
+                            self.after_batch_eval()
+                self.after_epoch()
+            logger.info('Returns successfully')
+            self.teardown()
+        except BaseException as err:
+            logger.error('Exception raised: {}'.format(err))
+            self.teardown(error=err)
+            raise
+
+    # noinspection PyUnresolvedReferences
+    def _organize_stats(self, stats: Tuple[Any]) -> dict:
+        logger = logging.getLogger(type(self).__name__ + '._organize_stats')
+        try:
+            stat_names = self.stat_names
+        except AttributeError:
+            # to conform to the naming policy of ``numpy.savez``
+            stat_names = ['arr_{}'.format(i) for i in range(len(stats))]
+
+        if len(self.stat_names) != len(stats):
+            logger.warning('len(self.stat_names) is '
+                           'different from len(stats): {} != {}'
+                           .format(len(self.stat_names), len(stats)))
+        return collections.OrderedDict(zip(stat_names, stats))
