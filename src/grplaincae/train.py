6364a39c08baa1692856d0ab228aa3b07de692d6
Kaiwen Wu <kps6326@hotmail.com>
2018-11-16 15:28:07 -0800

    Simplified API and moved some unrelated function from `train` to `viz`

---
+++ src/grplaincae/train.py
@@ -0,0 +1,170 @@
+import importlib
+import logging
+import os
+from typing import Iterable, Tuple, Sequence, Callable
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+import torchvision.transforms as trans
+import matplotlib
+
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+from utils import loggername as _l
+import vmdata
+import more_sampler
+import more_trans
+from gradreg import gradreg, no_grad_params
+from grplaincae.basicmodels import Autoencoder
+from trainlib import BasicTrainer
+
+
+class TrainOnlyAdamTrainer(BasicTrainer):
+    """
+    No evaluation loop. Train for at most one epoch. Use Adam optimizer at
+    initial learning rate ``TrainOnlyAdamTrainer.optim_lr`` (1e-4).
+    """
+    optim_lr = 1e-4
+
+    def __init__(self, net_module, root: str, transform,
+                 trainset_indices: Sequence[int], gr_strength=0.0,
+                 max_epoch: int = 1, batch_size: int = 8,
+                 device='cpu'):
+        logger = logging.getLogger(_l(__name__, self, '__init__'))
+        self.net_module = net_module
+        net = Autoencoder(self.net_module.STCAEEncoder(), self.net_module.STCAEDecoder())
+        logger.debug('Loaded autoencoder net from module {}'
+                     .format(self.net_module.__name__))
+
+        super().__init__(net, max_epoch=max_epoch, device=device)
+        self.vdset = vmdata.VideoDataset(root, transform=transform)
+        self.criterion = nn.MSELoss().to(device)
+        self.optimizer = optim.Adam(self.net.parameters(),
+                                    lr=type(self).optim_lr)
+        self.trainset_indices = trainset_indices
+        self.gr_strength = gr_strength
+        self.batch_size = batch_size
+
+        self.stat_names = ('loss',)
+        self.train_stages = ('train',)
+        self.train_batch_sampler_ = lambda: more_sampler.SlidingWindowBatchSampler(
+                self.trainset_indices, 1 + self.net_module.temporal_batch_size,
+                batch_size=batch_size, shuffled=(self.max_epoch > 1))
+
+    # noinspection PyAttributeOutsideInit
+    def before_epoch(self):
+        self.train_batch_sampler = self.train_batch_sampler_()
+
+    def get_trainloader(self) -> Iterable[Tuple[torch.Tensor, torch.Tensor]]:
+        dataloader = DataLoader(self.vdset,
+                                batch_sampler=self.train_batch_sampler)
+        for frames in dataloader:
+            frames = more_trans.rearrange_temporal_batch(frames, 1 + self.net_module.temporal_batch_size)
+            inputs, targets = frames[:, :, :-1, :, :], frames[:, :, -1:, :, :]
+            inputs, targets = inputs.to(self.device), targets.to(self.device)
+            yield inputs, targets
+
+    def train_once(self, inputs, targets):
+        """
+        Train for one minibatch.
+        """
+        logger = logging.getLogger(_l(__name__, self, 'train_once'))
+        loss_part = [0, 0]
+        with gradreg(inputs, strength=self.gr_strength) as ns:
+            outputs = self.net(inputs)
+            ns.loss = self.criterion(outputs, targets)
+            loss_part[0] = ns.loss.item()
+        self.optimizer.zero_grad()
+        ns.loss.backward()
+        self.optimizer.step()
+        loss_part[1] = ns.loss.item() - loss_part[0]
+        logger.debug('loss part: {}'.format(tuple(loss_part)))
+        return ns.loss.item(),
+
+    def eval_once(self, inputs, targets):
+        """
+        Evaluate for one minibatch.
+        """
+        with no_grad_params(self.net):
+            with gradreg(inputs, strength=self.gr_strength, train=False) as ns:
+                outputs = self.net(inputs)
+                ns.loss = self.criterion(outputs, targets)
+        return ns.loss.item(),
+
+    def teardown(self, error=None):
+        self.vdset.release_mmap()
+    #
+    # def predict(self, indices: Sequence[int], todir: str,
+    #             oob_policy: Callable[[torch.Tensor], torch.Tensor]
+    #             = more_trans.clamp_tensor_to_image):
+    #     """
+    #     :param indices: indices used to make predictions from
+    #     :param oob_policy: a function that converts each element of a tensor to
+    #            within range [0, 1]
+    #     :return:
+    #     """
+    #     logger = logging.getLogger(_l(__name__, self, 'predict'))
+    #     sam = more_sampler.SlidingWindowBatchSampler(
+    #             indices, self.net_module.temporal_batch_size,
+    #             batch_size=self.batch_size, shuffled=False)
+    #     os.makedirs(todir, exist_ok=True)
+    #     loader = DataLoader(self.vdset, batch_sampler=sam)
+    #     try:
+    #         with torch.no_grad():
+    #             for batch, inputs in enumerate(loader):
+    #                 inputs = more_trans.rearrange_temporal_batch(
+    #                         inputs, self.net_module.temporal_batch_size)
+    #                 inputs = inputs.to(self.device)
+    #                 outputs = self.net(inputs)
+    #                 preds = self.net_module.outputs_as_images(outputs, oob_policy)
+    #                 for j, img in enumerate(preds):
+    #                     tofile = os.path.join(todir, 'p{}_{}.png'.format(batch, j))
+    #                     plt.imsave(tofile, img.detach().cpu().numpy())
+    #                     plt.close()
+    #                     logger.debug('Written prediction to file "{}"'
+    #                                  .format(tofile))
+    #     finally:
+    #         self.vdset.release_mmap()
+    #
+    # def predict_with_attn(self, indices: Sequence[int], todir: str,
+    #                       oob_policy: Callable[[torch.Tensor], torch.Tensor]
+    #                       = more_trans.clamp_tensor_to_image):
+    #     logger = logging.getLogger(_l(__name__, self, 'predict_with_attn'))
+    #     sam = more_sampler.SlidingWindowBatchSampler(
+    #             indices, self.net_module.temporal_batch_size + 1,
+    #             batch_size=self.batch_size, shuffled=False)
+    #     os.makedirs(todir, exist_ok=True)
+    #     loader = DataLoader(self.vdset, batch_sampler=sam)
+    #     sigmoid = nn.Sigmoid()
+    #     try:
+    #         with no_grad_params(self.net):
+    #             for batch, frames in enumerate(loader):
+    #                 frames = more_trans.rearrange_temporal_batch(
+    #                         frames, self.net_module.temporal_batch_size + 1)
+    #                 inputs, targets = frames[:, :, :-1, :, :], frames[:, :, -1:, :, :]
+    #                 inputs, targets = inputs.to(self.device), targets.to(self.device)
+    #                 inputs.requires_grad_()
+    #                 outputs = self.net(inputs)
+    #                 loss = self.criterion(outputs, targets)
+    #                 loss.backward()
+    #                 attns = sigmoid(inputs.grad)
+    #
+    #                 outputs = self.net_module.outputs_as_images(outputs, oob_policy)
+    #                 attns = self.net_module.inputs_as_images(attns, lambda x: x)
+    #                 for j, (o, a) in enumerate(zip(outputs, attns)):
+    #                     tofile_o = os.path.join(todir, 'p{}_{}.png'.format(batch, j))
+    #                     plt.imsave(tofile_o, o.detach().cpu().numpy())
+    #                     plt.close()
+    #                     logger.debug('Written prediction to file "{}"'
+    #                                  .format(tofile_o))
+    #
+    #                     tofile_a = os.path.join(todir, 'a{}_{}.png'.format(batch, j))
+    #                     plt.imsave(tofile_a, a.detach().cpu().numpy())
+    #                     plt.close()
+    #                     logger.debug('Written predicted attention to file "{}"'
+    #                                  .format(tofile_a))
+    #     finally:
+    #         self.vdset.release_mmap()
---
+++ src/grplaincae/viz.py
@@ -0,0 +1,183 @@
+import importlib
+import os
+from typing import Tuple, Sequence, List, Iterable
+
+import torch
+from more_sampler import SlidingWindowBatchSampler
+import numpy as np
+import matplotlib
+import vmdata
+from more_trans import rearrange_temporal_batch, DeNormalize, clamp_tensor_to_image
+from torch import nn
+from torch.utils.data import DataLoader
+
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+from grplaincae.basicmodels import Autoencoder, SpatioTemporalInputShape
+from trainlib import load_checkpoint
+
+unit_figsize = (6, 4)
+
+
+def load_trained_net(model_name: str, savedir: str, progress: Tuple[int, int]):
+    m = importlib.import_module('grplaincae.models.' + model_name)
+    encoder = m.STCAEEncoder()
+    decoder = m.STCAEDecoder()
+    net = Autoencoder(encoder, decoder)
+    load_checkpoint(net, savedir, 'checkpoint_{}_{}.pth', progress)
+    net.eval()
+    for p in net.parameters():
+        p.requires_grad = False
+    return net, m
+
+
+def draw_left_right(tofile: str, arrs: Tuple[np.ndarray, np.ndarray],
+                    margin: int = 1, init_color=0) -> None:
+    """
+    Draw grayscale image.
+    """
+    arr1, arr2 = arrs
+    (h1, w1), (h2, w2) = arr1.shape, arr2.shape
+    h, w = max(h1, h2), w1 + margin + w2
+    dt1, dt2 = str(arr1.dtype), str(arr2.dtype)
+    dt = np.uint8 if ('int' in dt1 and 'int' in dt2) else np.float64
+
+    figsize = (unit_figsize[0], unit_figsize[1] * 2)
+    plt.figure(figsize=figsize)
+    canvas = (init_color * np.ones((h, w))).astype(dt)
+    canvas[:h1, :w1] = arr1
+    canvas[:h2, -w2:] = arr2
+    plt.imsave(tofile, canvas)
+    plt.close()
+
+
+def draw_up_down(tofile: str, arrs: Tuple[np.ndarray, np.ndarray],
+                 margin: int = 1, init_color=0) -> None:
+    arr1, arr2 = arrs
+    (h1, w1), (h2, w2) = arr1.shape, arr2.shape
+    h, w = h1 + margin + h2, max(w1, w2)
+    dt1, dt2 = str(arr1.dtype), str(arr2.dtype)
+    dt = np.uint8 if ('int' in dt1 and 'int' in dt2) else np.float64
+
+    figsize = (unit_figsize[0] * 2, unit_figsize[1])
+    plt.figure(figsize=figsize)
+    canvas = (init_color * np.ones((h, w))).astype(dt)
+    canvas[:h1, :w1] = arr1
+    canvas[-h2:, :w2] = arr2
+    plt.imsave(tofile, canvas)
+    plt.close()
+
+
+def to_cpu(tensor: torch.Tensor) -> torch.Tensor:
+    return tensor.detach().cpu()
+
+
+def BCTHW2LoCHW(tensor: torch.Tensor) -> List[torch.Tensor]:
+    sh = SpatioTemporalInputShape(*tensor.size())
+    return list(tensor.reshape(sh.B * sh.T, sh.C, sh.H, sh.W))
+
+
+def LoCHW2BTHW(tensors: Iterable[torch.Tensor], T: int) -> torch.Tensor:
+    tensor = torch.stack(tuple(tensors))
+    tensor = rearrange_temporal_batch(tensor, T)
+    tensor = tensor[:, 0, :, :, :]
+    return tensor
+
+
+def postprocess(inputs: torch.Tensor, attns: torch.Tensor,
+                outputs: torch.Tensor, targets: torch.Tensor,
+                denormalize, tbatch_size) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+    inputs = to_cpu(inputs)  # shape: (B, 1, T, H, W)
+    attns = to_cpu(attns)  # shape: (B, 1, T, H, W)
+    outputs = to_cpu(outputs)  # shape: (B, 1, 1, H, W)
+    targets = to_cpu(targets)  # shape: (B, 1, 1, H, W)
+
+    inputs = BCTHW2LoCHW(inputs)
+    outputs = BCTHW2LoCHW(outputs)
+    targets = BCTHW2LoCHW(targets)
+
+    inputs = map(clamp_tensor_to_image, map(denormalize, inputs))
+    outputs = map(clamp_tensor_to_image, map(denormalize, outputs))
+    targets = map(clamp_tensor_to_image, map(denormalize, targets))
+
+    inputs = LoCHW2BTHW(inputs, tbatch_size)  # shape: (B, T, H, W)
+    attns = attns.squeeze(1)  # shape: (B, T, H, W)
+    outputs = LoCHW2BTHW(outputs, 1).squeeze(1)  # shape: (B, H, W)
+    targets = LoCHW2BTHW(targets, 1).squeeze(1)  # shape: (B, H, W)
+
+    inputs = inputs.numpy()
+    attns = attns.numpy()
+    outputs = outputs.numpy()
+    targets = targets.numpy()
+
+    return inputs, attns, outputs, targets
+
+
+def visualize(todir: str, root: str, transform, normalize_stats,
+              indices: Sequence[int],
+              net: nn.Module, net_module,
+              temperature: float = 1.0,
+              device: str = 'cpu',
+              batch_size: int = 1,
+              predname_tmpl: str = 'pred{}.png',
+              attnname_tmpl: str = 'attn{}_pred{}.png') -> None:
+    """
+    Visualize predictions and input gradients.
+
+    :param todir: directory under which to save the visualization
+    :param root: the dataset root
+    :param transform: transform on dataset inputs
+    :param normalize_stats: the mean-std tuple used in normalization
+    :param indices: indices of dataset inputs involved in visualization
+    :param net: the trained network
+    :param net_module: the module from which ``net`` is loaded, as returned by
+           ``load_trained_net``
+    :param temperature: the larger it is, the more contrast in the attention
+           map is
+    :param device: where to do inference
+    :param batch_size: batch size when doing inference; will be set to 1 if
+           ``device`` is 'cpu'
+    :param predname_tmpl: the basename template of prediction visualization
+    :param attnname_tmpl: the basename template of attention (inputs gradient)
+           visualization
+    """
+    tbatch_size = net_module.temporal_batch_size
+    if device == 'cpu':
+        batch_size = 1
+    sam = SlidingWindowBatchSampler(indices, 1 + tbatch_size, shuffled=False,
+                                    batch_size=batch_size, drop_last=True)
+    sam_ = SlidingWindowBatchSampler(indices, 1 + tbatch_size, shuffled=False,
+                                     batch_size=batch_size, drop_last=True)
+    denormalize = DeNormalize(*normalize_stats)
+    os.makedirs(todir, exist_ok=True)
+
+    mse = nn.MSELoss().to(device)
+    sigmoid = nn.Sigmoid().to(device)
+    net = net.to(device)
+    with vmdata.VideoDataset(root, transform=transform) as vdset:
+        loader = DataLoader(vdset, batch_sampler=sam)
+        for frames, iframes in zip(loader, iter(sam_)):
+            frames = rearrange_temporal_batch(frames, 1 + tbatch_size)
+            iframes = np.array(iframes).reshape((batch_size, 1 + tbatch_size))
+            inputs, targets = frames[:, :, :-1, :, :], frames[:, :, -1:, :, :]
+            iinputs, itargets = iframes[:, :-1], iframes[:, -1]
+            inputs, targets = inputs.to(device), targets.to(device)
+
+            inputs.requires_grad_()
+            outputs = net(inputs)
+            loss = mse(outputs, targets)
+            loss.backward()
+            attns = sigmoid(inputs.grad * temperature)
+
+            inputs, attns, outputs, targets = postprocess(
+                    inputs, attns, outputs, targets,
+                    denormalize, tbatch_size)
+
+            for b in range(batch_size):
+                f = os.path.join(todir, predname_tmpl.format(itargets[b]))
+                draw_left_right(f, (outputs[b], targets[b]))
+                for t in range(tbatch_size):
+                    f = os.path.join(todir, attnname_tmpl.format(
+                            iinputs[b, t], itargets[b]))
+                    draw_up_down(f, (inputs[b, t], attns[b, t]))
