f34bdf87089dcb581163b5c98ad140480b4c3221
kkew3 <kps6326@hotmail.com>
2018-12-21 22:22:14 -0800

    Added `make_grid` function that helps arrange numpy array images into grid for display. Added `H5ResultSaver` that aids in dumping results of large-scale experiments in memory- and IO-efficient manner.
    Other minor changes: coding style fixes by IDE.

--- lib/exprlib.py
+++ lib/exprlib.py
@@ -1,23 +1,29 @@
 """
 Common utilities used in experiments.
 """
-
+import pdb
+import functools
+import importlib
+import itertools
+import collections
 import os
 import re
 import json
 import tempfile
 import contextlib
 from types import SimpleNamespace
+from typing import Union, Tuple, Sequence, Optional
 
 import numpy as np
+import h5py
 
 
 class ExperimentLauncher(object):
     """
     Launch experiment of which the results are to be stored in a single file.
     Returns the results directly if the experiment has been launched before
-    with the same arguments; otherwise run the experiment and save the results.
-    The experiment parameters are encoded in JSON string.
+    with the same arguments; otherwise run the experiment and save the
+    results. The experiment parameters are encoded in JSON string.
 
     Attributes:
 
@@ -27,7 +33,8 @@ class ExperimentLauncher(object):
     def __init__(self, basedir, prefix='', suffix='', translate_kwargs=str):
         """
         :param basedir: the base directory to load/store results
-        :param prefix: the prefix of the stored result and parameter JSON file
+        :param prefix: the prefix of the stored result and parameter JSON
+               file
         :param suffix: the suffix of the stored result file; e.g. the file
                extension name
         :param translate_kwargs: a function that takes in the value of one
@@ -127,24 +134,23 @@ def fig_as_data(plt, fig, ax, with_alpha=False):
         fig.canvas.draw()
         data = np.array(fig.canvas.renderer._renderer)
         if not with_alpha:
-            data = data[...,:3]
+            data = data[..., :3]
         ns.data = data
     finally:
         plt.close()
 
 
-def get_runid_from_file(_file_: str, return_prefix=False) -> str:
+def get_runid_from_file(_file_: str, return_prefix=False) -> Union[str, Tuple[str, str]]:
     r"""
     Extract runid from __file__, provided that __file__ is of format
     ``{something}.{runid}.py``, strictly speaking, of regex
     ``^[^\n\t\.]*\.([^\n\t\.]+)\.py$``.
 
-    >>> get_runid_from_file
     :param _file_: the ``__file__`` constant
     :param return_prefix: if True, also returns the ``something`` part
-    :return: the runid
+    :return: the runid, and possibly ``something``
     """
-    pattern = r'^([^\n\t\.]*)\.([^\n\t\.]+)\.py$'
+    pattern = r'^([^\n\t\.]*)\.([^\n\t\.]+)\.(py|ini)$'
     matched = re.match(pattern, os.path.basename(_file_))
     if not matched:
         raise ValueError('"{}" not matching pattern \'{}\''
@@ -153,3 +159,154 @@ def get_runid_from_file(_file_: str, return_prefix=False) -> str:
         return matched.group(2), matched.group(1)
     else:
         return matched.group(2)
+
+
+def make_grid(images: Sequence[np.ndarray], layout: Sequence[Sequence[int]],
+              margin: int = 1) -> np.ndarray
+    """
+    Make a sequence of images into grid so that they can be plotted in one
+    figure. The grid use matrix coordinate, 0-indexed.
+
+    :param images: the images, of dtype either ``np.uint8`` or ``np.float64``
+    :param layout: 2D int array to specify the grid layout; the ``layout[i,j]``
+           ``images[layout[i,j]]`` should be the image to be shown at
+           ``(i,j)`` coordinate of the grid. If ``layout[i,j]`` is -1, that
+           cell will be left empty
+    :param margin: the width of margin between cells
+    :return: the grid
+    :raise ValueError: if ``images`` is empty
+    """
+    if not len(images):
+        raise ValueError('No image to make grid')
+
+    layout = np.array(layout, dtype=np.int64)
+    cell_sizes = np.ones((2,) + layout.shape, dtype=np.int64)
+    for i, j in itertools.product(*map(range, layout.shape)):
+        if layout[i, j] >= 0:
+            cell_sizes[:, i, j] = images[layout[i, j]].shape[:2]
+    nrows = np.max(cell_sizes[0], axis=1)
+    ncols = np.max(cell_sizes[1], axis=0)
+    nchannels = max(iter((im.shape[2] if len(im.shape) == 3 else 1)
+                         for im in images))
+    canvas = np.zeros((sum(nrows) + margin * (len(nrows) - 1),
+                       sum(ncols) + margin * (len(ncols) - 1),
+                       nchannels),
+                      dtype=images[0].dtype)
+    for i, j in itertools.product(*map(range, layout.shape)):
+        if layout[i, j] >= 0:
+            loc = (np.sum(nrows[:i]) + i * margin,
+                   np.sum(ncols[:j]) + j * margin)
+            im = images[layout[i, j]]
+            if len(im.shape) == 2:
+                im = im[..., np.newaxis]
+            if 1 == im.shape[2] < nchannels:
+                im = np.repeat(im, nchannels, axis=2)
+            canvas[loc[0]:loc[0]+im.shape[0], loc[1]:loc[1]+im.shape[1]] = im
+    return canvas
+
+
+class H5ResultSaver(object):
+    """
+    Save large-scale experiment results in HDF5 file. This helps efficiently
+    store up to gigabytes of data without loading all at once to memory when
+    reading.
+
+    Assumptions about the experiment results:
+
+        - Each experiment produces k results, each of data type DT_k,
+          shape SH_k, and name NAME_k
+        - All results are presented as numpy arrays
+        - N experiments will be performed in total
+
+    The results are arranged in k databases, named NAME_k. Each database
+    is of shape (N, *SH_k). At ``__init__``, (DT_k, SH_k) of each NAME_k
+    must be specified.
+
+    It's important to ``close`` the ``H5ResultSaver``.  To close the saver
+    automatically, use ``contextlib.closing`` on the ``H5ResultSaver`` object.
+    """
+
+    #                               dtype shape
+    DsSpec = collections.namedtuple('DsSpec', ('dt', 'sh'))
+
+    def __init__(self, filename, config, **kwargs):
+        """
+        :param filename: the path of the HDF5 file to write
+        :param config: a dict of {NAME_k: (DT_k, SH_k)} as described
+               in ``help(H5ResultSaver)``
+
+        Keyword arguments
+        +++++++++++++++++
+
+        :param growth: the growth of the size of datasets when appending,
+               default to 100
+        :param initlen: the initial length of datasets at creation, default
+               to twice of ``growth``
+        :param overwrite: True to overwrite existing file, default to False
+        """
+        self.config = {}
+        for name, (dt, sh) in config.items():
+            dt = np.dtype(dt)
+            sh = tuple(map(int, sh))
+            if len(sh) and min(sh) < 0:
+                raise ValueError('Invalid shape: {}'.format(sh))
+            self.config[name] = H5ResultSaver.DsSpec(dt, sh)
+        self.growth = int(kwargs.get('growth', 100))
+        self.initlen = int(kwargs.get('initlen', 2 * self.growth))
+        self.dslen = 0
+
+        # fail if `filename` already exists
+        overwrite = bool(kwargs.get('overwrite', False))
+        mode = 'w' if overwrite else 'w-'
+        self.h5file = h5py.File(filename, mode)
+
+    def save(self, **results):
+        """
+        Append results to datasets.
+
+        :param results: dict of results {name: data}, where ``name`` must be
+               one of the NAME_k specified at ``__init__``,
+               ``np.stack(data).shape`` must be equal to SH_k, and
+               ``np.stack(data).dtype`` must be equal to DT_k,
+               ``len(results)`` must be equal to the size of config at
+               ``__init__``; otherwise, raise ``ValueError``
+        """
+        to_append = set(self.config)
+        self._ensure_capacity()
+        for name, data in results.items():
+            data = np.stack(data)
+            spec = self.config[name]
+            if data.shape != spec.sh or data.dtype != spec.dt:
+                raise ValueError('Invalid shape/dtype of data named "{}"'
+                                 .format(name))
+            self.h5file[name][self.dslen] = data
+            to_append.remove(name)
+        if len(to_append):
+            raise ValueError('Missing results: {}'.format(to_append))
+        self.dslen += 1
+
+    def close(self):
+        self._trim_to_length()
+        self.h5file.close()
+
+    def _ensure_capacity(self):
+        for name, spec in self.config.items():
+            try:
+                ds = self.h5file[name]
+            except KeyError:
+                ds = self.h5file.create_dataset(
+                    name, shape=(self.initlen,) + spec.sh,
+                    dtype=spec.dt, chunks=True, maxshape=(None,) + spec.sh,
+                    compression='gzip')
+            else:
+                while self.dslen >= len(ds):
+                    ds.resize(self.dslen + self.growth, axis=0)
+
+    def _trim_to_length(self):
+        for name in self.config:
+            try:
+                ds = self.h5file[name]
+            except KeyError:
+                pass
+            else:
+                ds.resize(self.dslen, axis=0)
